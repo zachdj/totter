38a0c964d65e6dc3c94fb02fbe66b8fb27c0f4cd
Zach Jones <zachydj@gmail.com>
2018-10-20 00:45:06 -0400

    Add ABC for qwop strategies

---
+++ totter/evolution/QwopStrategy.py
@@ -0,0 +1,38 @@
+""" Abstract base class representing QWOP strategies
+
+A QWOP Strategy is a sequence of keystrokes that plays QWOP.
+Each Strategy must implement an `execute` method, which executes the keystrokes for the strategy with the correct timing.
+When evaluating the strategy, `execute` will automatically be looped until `stop` is called.
+
+"""
+
+from abc import abstractmethod
+from multiprocessing import Process
+import time
+
+
+class QwopStrategy:
+    def __init__(self):
+        self.stopped = False
+
+    def stop(self):
+        self.stopped = True
+
+    def _run_execute(self):
+        while True:
+            self.execute()
+
+    def run(self):
+        # spawn a process to run `execute` on a loop
+        execution_process = Process(target=self._run_execute)
+        execution_process.start()
+
+        # check for the stopping condition every 250 ms
+        while not self.stopped:
+            time.sleep(0.25)
+
+        execution_process.terminate()
+
+    @abstractmethod
+    def execute(self):
+        pass
