58ab9c0c5474ed6653951ba9cc207dcd5ea93a01
Zach Jones <zachydj@gmail.com>
2018-10-21 12:54:35 -0400

    Change idea for app architecture

--- test.py
+++ test.py
@@ -6,21 +6,16 @@ from totter.evolution.QwopStrategy import QwopStrategy
 TODO: idea about app architecture:
     `evolution.algorithm` - ABC for a GA
     
-    - main process runs a `evolution.algorithm` GA.
-    - spawns population, does xover, mutation, etc
-    - When time for evaluation:
-        - create an `evolution.QwopStrategy` that represents the individual 
-        - spin up an `api.QwopEvaluator`
-            - QwopEvaluator create a QWOP webview in the main thread
-            - QwopEvaluator creates a different thread to start the game, then run the QwopStrategy
-            - QwopEvaluator creates a different thread to periodically check if the game is over
-                - when the game ends, thread 3 calls `stop` the QwopStrategy then waits for it to join
-                - thread 3 kills the webview then terminates
-        
-    - GA execution continues
+    - spin up a separate thread that runs the GA - does xover, mutation, etc
+    - in the main thread, create the qwop window
+    - when the GA thread wants to evaluate:
+        - translates individuals into `QwopStrategy` objects
+        - calls `QwopEvaluator.evaluate` with the strategies
+        - `evaluate` blocks thread execution while evaluating each individual
+            - each QwopStrategy executes until the game has ended
+            - when the game has ended, the result is saved, and the evaluator presses space to start a new game
     
-ALTERNATIVELY:
-    - QwopEvaluator could evaluate a set of QwopStrategies using a single QWOP instance
+    - the GA thread kills the webview when it's done
 
 """
 
