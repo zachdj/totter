8ee6863d7f56631ffc9bc468c24ff6c3745eed55
Zach Jones <zachydj@gmail.com>
2018-10-24 03:33:31 -0400

    Construct QwopStrategies with function

--- totter/evolution/QwopStrategy.py
+++ totter/evolution/QwopStrategy.py
@@ -1,17 +1,19 @@
-from abc import abstractmethod
 import pyautogui
 
 
 class QwopStrategy:
-    def __init__(self):
-        """ Abstract base class representing QWOP strategies
+    def __init__(self, execution_function):
+        """ Class representing QWOP strategies
 
         A QWOP Strategy is a sequence of keystrokes that plays QWOP.
         Each Strategy must implement an `execute` method, which executes the keystrokes for the strategy with the correct timing.
         When evaluating the strategy, `execute` will automatically be looped until the game ends.
 
+        Args:
+            execution_function (function): function that implements the strategy
+
         """
-        pass
+        self.execute = execution_function
 
     def cleanup(self):
         """ Cleans up after strategy execution
@@ -23,7 +25,3 @@ class QwopStrategy:
         # ensure all keys are up
         for key in ('q', 'w', 'o', 'p', 'space'):
             pyautogui.keyUp(key)
-
-    @abstractmethod
-    def execute(self):
-        pass
